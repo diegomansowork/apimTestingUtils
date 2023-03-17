6a2336c4f47a61ead478368ec7a1b8222d3e9bea
Diego Manso <diego.manso@inetum.com>
2023-03-17 14:06:29 +0100

    Commit inicial

---
+++ src/main/java/com/dmanso/apimTestingUtils/ApimTestingUtilsApplication.java
@@ -0,0 +1,13 @@
+package com.dmanso.apimTestingUtils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApimTestingUtilsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApimTestingUtilsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dmanso/apimTestingUtils/ApimTestingUtilsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmanso.apimTestingUtils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApimTestingUtilsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
